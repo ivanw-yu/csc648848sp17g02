643975eaa334eedf9ad759bd898d2d10ddc40a7e
jerrya <jauyeung91@gmail.com>
2017-05-08 04:59:44 -0700

    line 71 login after regitereing, in reg user control

--- src/Controller/RegisteredUsersController.php
+++ src/Controller/RegisteredUsersController.php
@@ -68,6 +68,7 @@ class RegisteredUsersController extends AppController
             {
                 $this->Flash->success(__('You are registered, welcome! Please login with your new credentials.'));
                 $this->RegisteredUsers->save($registeredUser);
+                $this->Auth->login();//login after registering
                 //$authUser = $this->RegisteredUsers->get($result->id)->toArray();//
                 //$this->Auth->setUser($authUser); //line for logging in after registering
                 return $this->redirect($this->referer());
